d70773f6e5af02cb402f4e77cdd1efb5ffd2416a
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:45:11 +0600

    first commit

---
+++ demoClass/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoClass
+{
+    class Person
+    {
+        public string name;
+        public void walk()
+        {
+            Console.WriteLine(name + " is walking");
+        }
+    }
+}
---
+++ demoClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.name = "Shanto";
+            person.walk();
+
+            person.name = "Robin";
+            person.walk();
+        }
+    }
+}
